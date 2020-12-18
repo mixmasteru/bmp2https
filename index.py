b989f2f431756ce1602957c80ff999f5548db2b6
Ulrich Pech <ulrichpech@gmail.com>
2020-12-18 14:19:33 +0100

    remove PIL

--- index.py
+++ index.py
@@ -1,29 +1,16 @@
-import json
-import datetime
 import base64
-from PIL import Image
-import io
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    img = Image.open("falcon9_pil.bmp")  # .convert("RGB")
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='BMP')
-    img_byte_arr = img_byte_arr.getvalue()
+    with open("falcon9_pil.bmp", "rb") as image:
+        f = image.read()
+        b = bytearray(f)
 
-    return respond(img_byte_arr)
-
-    # return {'statusCode': 200,
-    #        'body': json.dumps(data),
-    #        'headers': {'Content-Type': 'application/json'}}
+    return respond(b)
 
 
-def respond(img_byte_arr):
-    return {'isBase64Encoded': False,
+def respond(buf):
+    return {'isBase64Encoded': True,
             'statusCode': 200,
             'headers': {'Content-Type': 'image/bmp'},
-            'body': base64.b64encode(img_byte_arr).decode('UTF-8')}
+            'body': base64.b64encode(buf).decode('UTF-8')}

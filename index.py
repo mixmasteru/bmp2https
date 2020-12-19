fa7ffad710cbf4077537c0834bc85fd5224e1052
Ulrich Pech <ulrichpech@gmail.com>
2020-12-19 17:03:38 +0100

    add s3 param

--- index.py
+++ index.py
@@ -1,13 +1,24 @@
 import base64
+from boto3 import client
 
 
 def handler(event, context):
-
-    with open("falcon9_pil.bmp", "rb") as image:
-        f = image.read()
-        b = bytearray(f)
+    b = ""
+    if event.pathParameters.bucket:
+        bucket = event.pathParameters.bucket
+        s3bmp(bucket)
+    else:
+        with open("falcon9_pil.bmp", "rb") as image:
+            f = image.read()
+            b = bytearray(f)
 
     return respond(b)
+
+
+def s3bmp(bucket_name):
+    conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
+    for key in conn.list_objects(Bucket=bucket_name)['Contents']:
+        print(key['Key'])
 
 
 def respond(buf):

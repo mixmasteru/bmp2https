57911997ec93926e2cd496c48ca2abf4c6f21af0
Ulrich Pech <ulrichpech@gmail.com>
2020-12-20 17:52:59 +0100

    add s3 read policy

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,11 +6,12 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
+        # event = {'pathParameters': {'bucket': 'test'}}
         result = index.handler(None, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'image/bmp')
-        #self.assertIn('Hello World', result['body'])
+        # self.assertIn('Hello World', result['body'])
 
 
 if __name__ == '__main__':
